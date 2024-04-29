50c6c0a3772a61fa1dcda51f70db23d31ec871cc
soosung.lee <soosung.lee@samsung.com>
2024-04-29 12:20:05 +0900

    Baseball 01

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,7 @@
 public class Game {
+    public void guess(Object guessNumber) {
+        if(guessNumber==null){
+            throw new IllegalArgumentException();
+        }
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    void createGame(){
+        assertNotNull(game);
+    }
+
+    @Test
+    void throwExceptionWhenInputIsNull(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            game.guess(null);
+        });
+    }
 }
